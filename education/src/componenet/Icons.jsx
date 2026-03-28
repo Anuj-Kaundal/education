7b2ece086eb8363c034a88be6d3f8225b952d5ce
Anuj-Kaundal <kaundalanuj45@gmail.com>
2026-03-28 13:56:24 +0530

    feat: enhance Login component with login/logout functionality and improve UI structure

--- education/src/componenet/Icons.jsx
+++ education/src/componenet/Icons.jsx
@@ -5,10 +5,11 @@ function Icons() {
     return (
         <div>
             <div className='fixed right-5 bottom-5 flex flex-col gap-3'>
-                <div className='w-14 h-14 bg-blue-500 rounded-full flex items-center justify-center cursor-pointer text-2xl text-white' onClick={() => window.open('tel:+918091770596')}>
+                <div className='w-14 h-14 bg-blue-500 rounded-full flex items-center justify-center cursor-pointer text-2xl text-white' onClick={() => window.open('tel:+9189798 91708')}>
                     <FaPhone />
                 </div>
-                <div className='w-14 h-14 bg-green-500 rounded-full flex items-center justify-center cursor-pointer text-3xl text-white' onClick={() => window.open('https://wa.me/8091770596')}>
+                <div className='w-14 h-14 bg-green-500 rounded-full flex items-center justify-center cursor-pointer text-3xl text-white'
+                    onClick={() => window.open('https://wa.me/918979891708')}>
                     <FaWhatsapp />
                 </div>
             </div>
